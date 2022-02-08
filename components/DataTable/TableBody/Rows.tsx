e523fcfe1fefc26641ccedf0a8eb8ff0d1f71179
Jason Rice <jrice@entrata.com>
2022-02-07 22:47:18 -0700

    General cleanup. Please test locally. Submitting a pull request.

--- components/DataTable/TableBody/Rows.tsx
+++ components/DataTable/TableBody/Rows.tsx
@@ -1,54 +1,61 @@
-import { useAppContext } from '../../../context/AppProvider'
+import {useAppContext} from '../../../context/AppProvider'
 import * as React from 'react'
 import * as Types from '../../../types/rpg-types'
 
 interface RowsProps {
-    rows: Array<object>,
-    page: number,
-    pageSize:number,
-    header:string
+  rows: Array<object>,
+  page: number,
+  pageSize: number,
+  header: string
 }
 
-export default function Rows({rows,page,pageSize,header}:RowsProps) {
-    const {state:{creatures}, dispatch} = useAppContext()
-    let content: Array<React.ReactNode> = []
-    let tableSize:number = rows.length
-    let pageStart:number = page === 1 ? 0 : (page-1) * pageSize
-    let pageEnd = pageStart + pageSize <= tableSize ? pageStart + pageSize : tableSize;
-    const pageOfRows = rows.slice(pageStart,pageEnd);
-    
-    const setRecID = (event:any)=>{
-        let recID = event.currentTarget.children[0].innerText
-        recID = parseInt(recID)
-        dispatch(Types.SET_CREATURE_ID,recID)
-    }
+export default function Rows({rows, page, pageSize, header}: RowsProps) {
+  const {dispatch} = useAppContext()
 
-    return pageOfRows.map((row:any, rowIndex: number)=>{
-        let rowChildren:JSX.Element[] = []
-        let rowProps:Object = {
-            className:"rowStyle",
-            id:`row-id-${rowIndex}`,
-            onClick:setRecID
-        }
+  let tableSize: number = rows.length
+  let pageStart: number = page === 1 ? 0 : (page - 1) * pageSize
+  let pageEnd = pageStart + pageSize <= tableSize ? pageStart + pageSize : tableSize
+  const pageOfRows = rows.slice(pageStart, pageEnd)
 
-        return (
+  const setRecID = (event: React.MouseEvent<HTMLDivElement>) => {
+    // Replaced innerText with innerHTML since the HTMLDivElement doesn't support innerText (this should work)
+    const recID: number = parseInt(event.currentTarget.children[0].innerHTML)
+    dispatch(Types.SET_CREATURE_ID, recID)
+  }
+
+  return (
+    <>
+      {
+        pageOfRows.map((row: any, rowIndex: number) => {
+          let rowProps: Object = {
+            className: 'rowStyle',
+            id: `row-id-${rowIndex}`,
+            onClick: setRecID
+          }
+
+          return (
             <div {...rowProps} key={`row-key-${rowIndex}`}>
-                {
-                    Object.keys(row).map((key: any, cellIndex: number) => {
-                        let cellProps:Object = {
-                            className:"cellStyle",
-                            id:`cell-id-${rowIndex}.${cellIndex}`,
-                        }
+              {
+                Object.keys(row).map((key: any, cellIndex: number) => {
+                  let cellProps: Object = {
+                    className: "cellStyle",
+                    id: `cell-id-${rowIndex}.${cellIndex}`,
+                  }
 
-                        return <span {...cellProps} key={`cell-key-${rowIndex}.${cellIndex}`}>{row[key]}</span>
-                    })
-                }
+                  return (
+                    <span
+                      {...cellProps}
+                      key={`cell-key-${rowIndex}.${cellIndex}`}
+                    >
+                      {row[key]}
+                    </span>
+                  )
+                })
+              }
             </div>
-        )
-
-        // let rowDiv = React.createElement("div",rowProps,rowChildren)
-        // content.push(rowDiv)
-    })
-
-    // return content;
+          )
+        })
+      }
+    </>
+  )
 }
--- components/DataTable/TableBody/utils.tsx
+++ components/DataTable/TableBody/utils.tsx
@@ -1,141 +1,141 @@
 import * as React from 'react'
-import { useAppContext } from '../../../context/AppProvider'
+import {useAppContext} from '../../../context/AppProvider'
 
 interface AnyObject {
-    [key: string]: any
-  }
+  [key: string]: any
+}
 
-interface AnyObjArray extends Array<AnyObject>{}
+interface AnyObjArray extends Array<AnyObject> {
+}
 
-const addIndexColumn = (rows:AnyObjArray)=>{
-    let recCount = 0
-    let tempArray: AnyObjArray=[]
-    rows.forEach((row)=>{
-      if (!(row==null)){
-        var newRow:AnyObject = {}
-        newRow["recID"] = recCount
-        for (const [key,value] of Object.entries(row)){
-          newRow[key] = value
-        }
-        tempArray.push(newRow)
-        recCount += 1
+const addIndexColumn = (rows: AnyObjArray) => {
+  let recCount = 0
+  let tempArray: AnyObjArray = []
+  rows.forEach((row) => {
+    if (!(row == null)) {
+      let newRow: AnyObject = {}
+      newRow["recID"] = recCount
+      for (const [key, value] of Object.entries(row)) {
+        newRow[key] = value
       }
+      tempArray.push(newRow)
+      recCount += 1
+    }
+  })
+  return tempArray
+}
+
+const sortColumn = (rows: AnyObjArray, column: number, direct: number) => {
+  const tempRows = rows.sort(function (a, b) {
+    const keys = Object.keys(a)
+    console.log(keys)
+    let keyA = a[keys[column]],
+      keyB = b[keys[column]];
+    // Compare the 2 dates
+    if (keyA < keyB) return -(direct);
+    if (keyA > keyB) return direct;
+    return 0;
+  });
+  return tempRows
+
+}
+
+const parseDataForTable = (data: Array<AnyObject>, columnKeys: Array<string>) => {
+  let newData: Array<AnyObject> = []
+  newData = data.map(row => {
+    let rowObj: AnyObject = {}
+    columnKeys.forEach(key => {
+      rowObj[key] = row[key]
     })
-    return tempArray
-}
-
-const sortColumn = (rows:AnyObjArray,column:number,direct:number)=>{
-    const tempRows = rows.sort(function(a, b) {
-                        const keys = Object.keys(a)
-                        console.log(keys)
-                        var keyA = a[keys[column]],
-                        keyB = b[keys[column]];
-                        // Compare the 2 dates
-                        if (keyA < keyB) return -(direct);
-                        if (keyA > keyB) return direct;
-                        return 0;
-                    });
-    return tempRows
-
-}
-
-const parseDataForTable = (data:Array<AnyObject>,columnKeys:Array<string>)=>{
-  let newData:Array<AnyObject>=[]
-  newData = data.map(row=>{
-      let rowObj:AnyObject = {}
-      columnKeys.forEach(key=>{
-        rowObj[key] = row[key]
-      })
-      return rowObj
+    return rowObj
   })
   return newData
 }
 
-const renderHeader = (row:Object)=> {
-    let content: JSX.Element[] = [];
-    const keys = Object.keys(row)
-    keys.map(key=>{
-        content.push( 
-            <span className="cellStyle" key={`row-${key}`} >{key}</span>
-            )
-    })
-    return content;
+const renderHeader = (row: Object) => {
+  let content: JSX.Element[] = [];
+  const keys = Object.keys(row)
+  keys.map(key => {
+    content.push(
+      <span className="cellStyle" key={`row-${key}`}>{key}</span>
+    )
+  })
+  return content;
 }
 
-function propertiesToArray(obj:object) {
-  const isObject = (val:any) =>
+function propertiesToArray(obj: object) {
+  const isObject = (val: any) =>
     val && typeof val === 'object' && !Array.isArray(val);
 
-  const addDelimiter = (a:string, b:string) =>
+  const addDelimiter = (a: string, b: string) =>
     a ? `${a}.${b}` : b;
 
-  const paths:any = (obj:object = {}, head = '') => {
+  const paths: any = (obj: object = {}, head = '') => {
     return Object.entries(obj)
-      .reduce((product:any, [key, value]) => 
-        {
-          let fullPath:string = addDelimiter(head, key)
-          return isObject(value) ?
-            product.concat(paths(value, fullPath))
+      .reduce((product: any, [key, value]) => {
+        let fullPath: string = addDelimiter(head, key)
+        return isObject(value) ?
+          product.concat(paths(value, fullPath))
           : product.concat(fullPath)
-        }, []);
+      }, []);
   }
 
   return paths(obj);
 }
 
-function deepCopy(obj:any):any {
+function deepCopy(obj: any): any {
   return Object.keys(obj).reduce((v, d) => Object.assign(v, {
-  [d]: (obj[d].constructor === Object) ? deepCopy(obj[d]) : obj[d]
+    [d]: (obj[d].constructor === Object) ? deepCopy(obj[d]) : obj[d]
   }), {});
 }
 
-const iterateObjEntries = (parent:string,val:any,objPath:Array<string>)=>{
-  let parentPath = parent==="" ? "" : parent 
-  if (Array.isArray(val)){
+const iterateObjEntries = (parent: string, val: any, objPath: Array<string>) => {
+  let parentPath = parent === "" ? "" : parent
+  if (Array.isArray(val)) {
     let index = 0
-    if (val.length === 0){
+    if (val.length === 0) {
       objPath.push(parent)
     }
-    val.forEach((subVal:any)=>{
+    val.forEach((subVal: any) => {
       let arrayIndex = "[" + index + "]"
-      if (typeof subVal === 'object'){
-        if (!(Array.isArray(subVal))){
-          Object.keys(subVal).forEach((key)=>{
+      if (typeof subVal === 'object') {
+        if (!(Array.isArray(subVal))) {
+          Object.keys(subVal).forEach((key) => {
             let value = subVal[key]
             if (value === null) {
               value = "null"
             }
-            iterateObjEntries(parentPath + arrayIndex + "." + key,value,objPath)
+            iterateObjEntries(parentPath + arrayIndex + "." + key, value, objPath)
           })
         } else {
-          iterateObjEntries(parentPath + arrayIndex,subVal,objPath)
+          iterateObjEntries(parentPath + arrayIndex, subVal, objPath)
         }
       } else {
         objPath.push(parentPath + arrayIndex)
       }
-      index +=1
+      index += 1
     })
   } else {
-    let dotOrNot = parentPath==="" ? "" : "." 
-    if (typeof val === 'object'){
-        Object.keys(val).forEach((key)=>{
-          let value = val[key]
-          if (value === null) {
-            value = "null"
-          }
-        iterateObjEntries(parent+dotOrNot+key,value,objPath)
+    let dotOrNot = parentPath === "" ? "" : "."
+    if (typeof val === 'object') {
+      Object.keys(val).forEach((key) => {
+        let value = val[key]
+        if (value === null) {
+          value = "null"
+        }
+        iterateObjEntries(parent + dotOrNot + key, value, objPath)
       })
     } else {
       objPath.push(parent)
     }
-  } 
+  }
 }
 
-const stringToPath = function (path:any) {
+const stringToPath = function (path: any) {
   // If the path isn't a string, return it
   if (typeof path !== 'string') return path;
   // Create new array
-  var output:Array<string> = [];
+  let output: Array<string> = [];
   // Split to an array with dot notation
   path.split('.').forEach(function (item, index) {
     // Split to an array with bracket notation
@@ -150,44 +150,54 @@ const stringToPath = function (path:any) {
 };
 
 
-const getObjValue = function (obj:any, path:string, def:any) {
-	/**
-	 * If the path is a string, convert it to an array
-	 * @param  {String|Array} path The path
-	 * @return {Array}             The path array
-	 */
-	// Get the path as an array
-	path = stringToPath(path);
-	// Cache the current object
-	var current = obj;
-	// For each item in the path, dig into the object
-	for (var i = 0; i < path.length; i++) {
-		// If the item isn't found, return the default (or null)
-		if (!current[path[i]]) return def;
-		// Otherwise, update the current value with added path element
-		current = current[path[i]];
-	}
-	return current;
+const getObjValue = function (obj: any, path: string, def: any) {
+  /**
+   * If the path is a string, convert it to an array
+   * @param  {String|Array} path The path
+   * @return {Array}             The path array
+   */
+  // Get the path as an array
+  path = stringToPath(path);
+  // Cache the current object
+  let current = obj;
+  // For each item in the path, dig into the object
+  for (let i = 0; i < path.length; i++) {
+    // If the item isn't found, return the default (or null)
+    if (!current[path[i]]) return def;
+    // Otherwise, update the current value with added path element
+    current = current[path[i]];
+  }
+  return current;
 };
 
-const setObjValue = function (obj:any, path:string, value:any) {
-	/**
-	 * If the path is a string, convert it to an array
-	 * @param  {String|Array} path The path
-	 * @return {Array}             The path array
-	 */
-	// Get the path as an array
-	path = stringToPath(path);
-	// Cache the current object
-  var current = obj
+const setObjValue = function (obj: any, path: string, value: any) {
+  /**
+   * If the path is a string, convert it to an array
+   * @param  {String|Array} path The path
+   * @return {Array}             The path array
+   */
+  // Get the path as an array
+  path = stringToPath(path);
+  // Cache the current object
+  let current = obj
   let j = 0
-	// For each item in the path, dig into the object
-  while (j<path.length-1) {
+  // For each item in the path, dig into the object
+  while (j < path.length - 1) {
     current = current[path[j]];
-    j +=1
+    j += 1
   }
   current[path[j]] = (!current[path[j]]) ? null : value
 }
 
 
-export {addIndexColumn,sortColumn,renderHeader,parseDataForTable,propertiesToArray,iterateObjEntries,getObjValue,setObjValue,renderTableRows,deepCopy}
+export {
+  addIndexColumn,
+  sortColumn,
+  renderHeader,
+  parseDataForTable,
+  propertiesToArray,
+  iterateObjEntries,
+  getObjValue,
+  setObjValue,
+  deepCopy
+}
