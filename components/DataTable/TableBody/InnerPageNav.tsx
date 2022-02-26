af5e313b1be70d7b229cc48e17e770db48d6a81f
Jason Rice <jrice@entrata.com>
2022-02-25 23:32:12 -0700

    Migrated conditional logic to the TSX view-layer. There's 10 more lines of code but that's mostly due to comments and single lined parens and curly braces (not a big deal). This is the React approach to combining conditional logic with the view layer (which at times is necessary).

--- components/DataTable/TableBody/InnerPageNav.tsx
+++ components/DataTable/TableBody/InnerPageNav.tsx
@@ -1,48 +1,94 @@
 import * as React from 'react'
 
 interface InnerNavProps {
-    lowerBound: number
-    upperBound: number
-    num:number
-    span:number
-    cur:number
-    pageHandler: React.MouseEventHandler
-  }
+  lowerBound: number
+  upperBound: number
+  num: number
+  span: number
+  cur: number
+  pageHandler: React.MouseEventHandler<HTMLButtonElement>
+}
 
-
+/**
+ * This component represents the view layer of the page navigation action bar.
+ * There's a lot of upper and lower bound computations in here along with some
+ * utility functions that might need to be abstracted out to utils/page-nav-utils.ts
+ * but for now this is fine.
+ *
+ * @param lowerBound
+ * @param upperBound
+ * @param num
+ * @param span
+ * @param cur
+ * @param pageHandler
+ * @constructor
+ */
 function InnerPageNav(
-    {
+  {
     lowerBound,
     upperBound,
     num,
     span,
     cur,
     pageHandler
-    }:InnerNavProps
-){
-    const ellipsis = "…"
-    let content = [];
-    //console.log(`"numPages: " ${num} "cur: " ${cur} "span: "${span}`)
+  }: InnerNavProps
+) {
+  // Utility functions (prior to render):
+  const ellipsis = "…"
+  const getLower = () => cur <= (num - span) ? cur - (Math.round(span / 2) - 1) : num - span
+  const getUpper = () => cur <= (num - span) ? cur + Math.round(span / 2) : num
 
-    if (num <= span || cur <= Math.round(span/2)){
-        upperBound = num <= span ? num : span
-        for (let i=1;i<=upperBound; i++) {
-            content.push(<button onClick={pageHandler} className="pageBox" key={i}>{i}</button>);
-        }
-    } else {
-        lowerBound = cur <= (num-span) ? cur - (Math.round(span/2)-1) : num - span
-        upperBound = cur <= (num-span) ? cur + Math.round(span/2) : num
-        for (let i=lowerBound;i<=upperBound; i++) {
-            content.push(<button onClick={pageHandler} className="pageBox" key={i}>{i}</button>);
-        }
-    }
-    if (num > span && upperBound < num){
-        content.push(<button className="pageBox" key={"ellipsis"}>{ellipsis}</button>);
-    }
+  /**
+   * This is really just a wrapper (syntatical sugar) for initializing an array and filling it.
+   *
+   * @param size of array to initialize
+   * @param fillVal defaults to undefined
+   */
+  const initArray = (size: number, fillVal: number | string | undefined = undefined) => {
+    return new Array(size)
+      .fill(fillVal)
+  }
 
-    return (<div>
-        </div>)
+  /**
+   * This function takes a start and end number and returns a range of numbers based on that.
+   * Ex: start => 5 end => 12 would yield [5, 6, 7, 8, 9, 10, 11, 12]
+   *
+   * @param start of range
+   * @param end of range
+   */
+  const range = (start: number, end: number): Array<number> => {
+    return initArray(end - start + 1)
+      .map((_, idx) => start + idx)
+  }
 
+  return (
+    <>
+      {
+        // IF
+        (num <= span || cur <= Math.round(span / 2)) ? (
+          initArray(num <= span ? num : span)
+            .map((val, idx) => (
+              <button onClick={pageHandler} className="pageBox" key={`main-${idx}`}>
+                {idx + 1}
+              </button>
+            ))
+        ) : (
+          // ELSE
+          range(getLower(), getUpper())
+            .map((val, idx) => (
+              <button onClick={pageHandler} className="pageBox" key={`secondary-${idx}`}>
+                {idx + 1}
+              </button>
+            ))
+        )
+      }
+      {
+        // IF
+        (num > span && upperBound < num) &&
+        <button className="pageBox" key={"ellipsis"}>{ellipsis}</button>
+      }
+    </>
+  )
 }
 
 export default InnerPageNav;
--- components/DataTable/TableBody/NewPageNavBar.tsx
+++ components/DataTable/TableBody/NewPageNavBar.tsx
@@ -2,118 +2,127 @@ import * as React from 'react'
 import InnerPageNav from './InnerPageNav';
 
 interface InputProps {
-    pageNums: number
-    setCurrentPage: Function
+  pageNums: number
+  setCurrentPage: Function
 }
 
 interface NavProps {
-    numPages: number
-    tableSpan: number
-    setCurrentPage: Function
-    page: number
-  }
+  numPages: number
+  tableSpan: number
+  setCurrentPage: Function
+  page: number
+}
 
 function NewPageNavBar(
-    {
-        numPages,
-        tableSpan,
-        setCurrentPage,
-        page
-    }:NavProps
+  {
+    numPages,
+    tableSpan,
+    setCurrentPage,
+    page
+  }: NavProps
 ) {
-    const [current, setCurrent] = React.useState(0);
-    const [lowerBound, setLowerBound] = React.useState<number>(1)
-    const [upperBound, setUpperBound] = React.useState<number>(1)
-    const start = "˂˂"
-    const down = "˂"
-    const up = "˃"
-    const end ="˃˃"
-    const ellipsis = "…"
-    const num = numPages
-    const cur = current
-    const span = tableSpan
-    const seq = [1,2,3,4,5,6,7,8]
+  const [current, setCurrent] = React.useState(0);
+  const [lowerBound, setLowerBound] = React.useState<number>(1)
+  const [upperBound, setUpperBound] = React.useState<number>(1)
+  const start = "˂˂"
+  const down = "˂"
+  const up = "˃"
+  const end = "˃˃"
+  const ellipsis = "…"
+  const num = numPages
+  const cur = current
+  const span = tableSpan
+  const seq = [1, 2, 3, 4, 5, 6, 7, 8]
 
-    const pageHandler =(event: any)=>{      
+  const pageHandler = (event: any) => {
 
-       if (!(isNaN(event.target.innerText))){
-            setCurrentPage(Number(event.target.innerText))
-            setCurrent(Number(event.target.innerText))
-        } else{
-          switch(event.target.id){
-              case "start": setCurrentPage(1); break;
-              case "down": setCurrent(current == 1 ? 1 : current -1);
-                           console.log ("down arrow page:" + current)
-                           setCurrentPage(current); 
-                           break;
-              case "up": setCurrent(current === numPages ? numPages : current + 1);
-                         setCurrentPage(current); 
-                         break;
-              case "end": setCurrentPage(numPages); break      
-          }   
-        }
+    if (!(isNaN(event.target.innerText))) {
+      setCurrentPage(Number(event.target.innerText))
+      setCurrent(Number(event.target.innerText))
+    } else {
+      switch (event.target.id) {
+        case "start":
+          setCurrentPage(1);
+          break;
+        case "down":
+          setCurrent(current == 1 ? 1 : current - 1);
+          console.log("down arrow page:" + current)
+          setCurrentPage(current);
+          break;
+        case "up":
+          setCurrent(current === numPages ? numPages : current + 1);
+          setCurrentPage(current);
+          break;
+        case "end":
+          setCurrentPage(numPages);
+          break
+      }
     }
+  }
 
-    const setBounds = ()=>{
-        if (numPages <= tableSpan || current <= Math.round(tableSpan/2)){
-            setLowerBound(1)
-            setUpperBound(numPages <= tableSpan ? numPages : tableSpan)
-        } else {
-            setLowerBound(current <= (numPages-tableSpan) ? cur - (Math.round(tableSpan/2)-1) : numPages - tableSpan)
-            setUpperBound(current <= (numPages-tableSpan) ? cur + Math.round(tableSpan/2) : numPages)
-        }
+  const setBounds = () => {
+    if (numPages <= tableSpan || current <= Math.round(tableSpan / 2)) {
+      setLowerBound(1)
+      setUpperBound(numPages <= tableSpan ? numPages : tableSpan)
+    } else {
+      setLowerBound(current <= (numPages - tableSpan) ? cur - (Math.round(tableSpan / 2) - 1) : numPages - tableSpan)
+      setUpperBound(current <= (numPages - tableSpan) ? cur + Math.round(tableSpan / 2) : numPages)
     }
+  }
 
-    const generatePageNav = () => {
-        let content = [];
-        let upperBound
-        let lowerBound
-        //console.log(`"numPages: " ${num} "cur: " ${cur} "span: "${span}`)
-        if (num <= span || cur <= Math.round(span/2)){
-            upperBound = num <= span ? num : span
-            for (let i=1;i<=upperBound; i++) {
-                content.push(<button onClick={pageHandler} className="pageBox" key={i}>{i}</button>);
-            }
-        } else {
-            lowerBound = cur <= (num-span) ? cur - (Math.round(span/2)-1) : num - span
-            upperBound = cur <= (num-span) ? cur + Math.round(span/2) : num
-            for (let i=lowerBound;i<=upperBound; i++) {
-                content.push(<button onClick={pageHandler} className="pageBox" key={i}>{i}</button>);
-            }
-        }
-        if (num > span && upperBound < num){
-            content.push(<button className="pageBox" key={"ellipsis"}>{ellipsis}</button>);
-        }
-        return content;
-      };
+  /**
+   * @See InnerPageNav
+   */
+  // const generatePageNav = () => {
+  //   let content = [];
+  //   let upperBound
+  //   let lowerBound
+  //   //console.log(`"numPages: " ${num} "cur: " ${cur} "span: "${span}`)
+  //   if (num <= span || cur <= Math.round(span / 2)) {
+  //     upperBound = num <= span ? num : span
+  //     for (let i = 1; i <= upperBound; i++) {
+  //       content.push(<button onClick={pageHandler} className="pageBox" key={i}>{i}</button>);
+  //     }
+  //   } else {
+  //     lowerBound = cur <= (num - span) ? cur - (Math.round(span / 2) - 1) : num - span
+  //     upperBound = cur <= (num - span) ? cur + Math.round(span / 2) : num
+  //     for (let i = lowerBound; i <= upperBound; i++) {
+  //       content.push(<button onClick={pageHandler} className="pageBox" key={i}>{i}</button>);
+  //     }
+  //   }
+  //   if (num > span && upperBound < num) {
+  //     content.push(<button className="pageBox" key={"ellipsis"}>{ellipsis}</button>);
+  //   }
+  //   return content;
+  // };
 
-      React.useEffect(()=>{
-        setCurrentPage(1)
-        setBounds()
-      },[])
+  React.useEffect(() => {
+    setCurrentPage(1)
+    setBounds()
+  }, [])
 
-      React.useEffect(()=>{
-        console.log(current)
-        setBounds()
-      },[current,numPages])
+  React.useEffect(() => {
+    console.log(current)
+    setBounds()
+  }, [current, numPages])
 
-    return (
-        <div className="PageNavBar">
-            <button id="start" onClick={pageHandler} className="pageBox" key={"start"}>{start}</button>
-            <button id="down" onClick={pageHandler} className="pageBox" key={"down"}>{down}</button>
-            <InnerPageNav
-                    lowerBound={lowerBound}
-                    upperBound={upperBound}
-                    num = {numPages}
-                    span ={tableSpan}
-                    cur ={current}
-                    pageHandler={pageHandler}
-            />
-            <button id="up" onClick={pageHandler} className="pageBox" key={"up"}>{up}</button>
-            <button id="end" onClick={pageHandler} className="pageBox" key={"end"}>{end}</button>
+  return (
+    <div className="PageNavBar">
+      <button id="start" onClick={pageHandler} className="pageBox" key={"start"}>{start}</button>
+      <button id="down" onClick={pageHandler} className="pageBox" key={"down"}>{down}</button>
+      <InnerPageNav
+        lowerBound={lowerBound}
+        upperBound={upperBound}
+        num={numPages}
+        span={tableSpan}
+        cur={current}
+        pageHandler={pageHandler}
+      />
+      <button id="up" onClick={pageHandler} className="pageBox" key={"up"}>{up}</button>
+      <button id="end" onClick={pageHandler} className="pageBox" key={"end"}>{end}</button>
 
-        </div> 
-    );
+    </div>
+  );
 }
 
 export default NewPageNavBar;
@@ -128,6 +137,6 @@ export default NewPageNavBar;
                     >
                     {num}
                     </button> )
-                }) 
+                })
             }
 */
