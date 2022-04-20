f0b9f9a8637260fb2aa177a800bba56587d44639
rlongo-dev <56315160+rlongo-dev@users.noreply.github.com>
2022-04-20 14:57:24 -0400

    custom image work

---
+++ components/ImageDisplay/CustomImage.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import * as types from '../../types/rpg-types'
+
+export default function CustomImage(){
+
+
+
+
+    return(
+        <div>
+            <div>
+                <image
+                
+                >
+                </image>
+            </div>
+        </div>
+    )
+}
