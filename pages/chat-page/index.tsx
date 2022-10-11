d1b111ba2b64c979294c50a2ce0e2458db4aeb2a
rlongo-dev <56315160+rlongo-dev@users.noreply.github.com>
2022-10-10 23:30:29 -0400

    fixed chat page

--- pages/chat-page/index.tsx
+++ pages/chat-page/index.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react'
+import UITopStatusBar from '../../components/blue-print/status-bar/ui-top-status-bar'
 import UIParent from '../../components/blue-print/ui-parent'
-import { useAppContext } from '../../context/app-provider'
+import useViewport from '../../hooks/useViewport'
+import { styleObj } from '../../styles/styles'
+import PlayerBP from '../../data/collections/maps/bp-player-dnd-5-1.0.json'
+import UINavbar from '../../components/blue-print/ui-nav-bar'
 
-function chatPage() {
+function ChatPage() {
+  const { devWidth, devHeight } = useViewport()
 
   return (
-    <div id="inventory-page">
-      <UIParent pageType={`chatPage`}/>
+    <div style={{ ...styleObj[`TopFlexPage`], height: `${devHeight}px`, width: `${devWidth}px` }}>
+      <UITopStatusBar {...PlayerBP.statusBar} />
+      <div className="overflow-control">
+        <UIParent pageType={`chatPage`}></UIParent>
+      </div>
+      <UINavbar />
     </div>
   )
 }
 
-export default chatPage
+export default ChatPage
